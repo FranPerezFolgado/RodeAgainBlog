367ee9857640f6ec62e7971c5d381bf6a50a9dd4
Fran Pérez <franperezfolgado@outlook.com>
2024-08-29 16:19:57 +0200

    added fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily:{
+			skyscappers: ["Skyscappers", "cursive"],
+      		blackdemo: ["Boiling-BlackDemo", "cursive"],
+      		commitmono: ["CommitMono-Regular-400", "monospace"],
+		}, 
 		extend: {},
 	},
-	plugins: [],
+	plugins: [],	
 }
+ 
