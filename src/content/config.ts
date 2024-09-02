59c13117e43fb69f2076814be98b5d8618548edb
Fran Pérez <franperezfolgado@outlook.com>
2024-09-02 15:36:22 +0200

    Added last post card

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
 import { defineCollection, z } from 'astro:content';
-
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+const posts = defineCollection({
+	type: "content",
+	schema: ({image}) =>
+	z.object({
+	  title: z.string(),
+	  pubDate: z.date(),
+	  description: z.string(),
+	  lastModified: z.string().optional(),
+	  cover: image(),
+	  coverAlt: z.string(),
+	  category: z.array(z.string()),
+	  tags: z.array(z.string()),
+	  author: z.string(),
+	  photosDir: z.string(),
 	}),
-});
+  });
 
-export const collections = { blog };
+export const collections = { posts };
