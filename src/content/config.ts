596206705a6990b6f5010eed108c26718ec53a8c
Fran Pérez <franperezfolgado@outlook.com>
2024-09-18 16:25:19 +0200

    added gallery view

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,29 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 const posts = defineCollection({
-	type: "content",
-	schema: ({image}) =>
-	z.object({
-	  title: z.string(),
-	  pubDate: z.date(),
-	  description: z.string(),
-	  lastModified: z.string().optional(),
-	  cover: image(),
-	  coverAlt: z.string(),
-	  category: z.array(z.string()),
-	  tags: z.array(z.string()),
-	  author: z.string(),
-	  monthDir: z.string(),
-	}),
-  });
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      lastModified: z.string().optional(),
+      cover: image(),
+      coverAlt: z.string(),
+      category: z.array(z.string()),
+      tags: z.array(z.string()),
+      author: z.string(),
+      monthDir: z.string(),
+    }),
+});
 
-export const collections = { posts };
+const photos = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      cover: image(),
+    }),
+});
+
+export const collections = { posts, photos };
